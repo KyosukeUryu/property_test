180a6f5134b1f924fe1784ba3f962f8ee431bc66
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-11 16:55:06 +0900

    登録機能実装

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -4,9 +4,17 @@ class PropertiesController < ApplicationController
   end
 
   def new
+    @property = Property.new
+    @property.nearest_stations.build
   end
 
   def create
+    @property = Property.new(property_params)
+    if @property.save
+      redirect_to properties_path, notice: '登録完了しました'
+    else
+      render :new
+    end
   end
 
   def show
@@ -20,5 +28,10 @@ class PropertiesController < ApplicationController
 
   def destroy
   end
-  
+
+  private
+
+  def property_params
+    params.require(:property).permit(:name, :rent, :address, :age, :remarks, nearest_stations_attributes:[:id, :route, :name, :on_foot])
+  end
 end
--- app/models/nearest_station.rb
+++ app/models/nearest_station.rb
@@ -2,5 +2,5 @@ class NearestStation < ApplicationRecord
   validates :route, presence: true, length: { maximum: 50 }
   validates :name, presence: true, length: { maximum: 30 }
   validates :on_foot, presence: true, numericality: { only_integer: true }
-  belongs_to :property
+  belongs_to :property, inverse_of: :nearest_stations
 end
