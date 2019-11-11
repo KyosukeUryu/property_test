a3e903d8589819b9c7cc73307ecbf48993f60412
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-11 13:53:02 +0900

    nearest_stationモデルバリデーション作成

--- app/models/nearest_station.rb
+++ app/models/nearest_station.rb
@@ -1,2 +1,6 @@
 class NearestStation < ApplicationRecord
+  validates :route, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :on_foot, presence: true, numericality: { only_integer: true }
+  belongs_to :property
 end
