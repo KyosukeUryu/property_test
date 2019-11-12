b6448e2c6a4f5659423653cde480256f6da799a4
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-12 10:30:34 +0900

    削除機能実装

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -34,14 +34,25 @@ class PropertiesController < ApplicationController
   end
 
   def update
+    if @property.update(update_property_params)
+      redirect_to properties_path, notice: '更新しました'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @property.destroy
+    redirect_to properties_path, notice: '削除しました'
   end
 
   private
 
   def property_params
+    params.require(:property).permit(:name, :rent, :address, :age, :remarks, nearest_stations_attributes:[:route, :name, :on_foot])
+  end
+
+  def update_property_params
     params.require(:property).permit(:name, :rent, :address, :age, :remarks, nearest_stations_attributes:[:id, :route, :name, :on_foot])
   end
 
